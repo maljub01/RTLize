1061927fd882cc33e314804f1be5289ece115bf3
Marwan Al Jubeh <marwan.aljubeh@gmail.com>
2012-10-08 22:46:55 +0300

    consistent stub numbers in tests

--- test/unit/rtlizer_test.rb
+++ test/unit/rtlizer_test.rb
@@ -26,9 +26,9 @@ class RtlizerTest < ActiveSupport::TestCase
     assert_declaration_transformation("border-left-style: solid;", "border-right-style: solid;")
     assert_declaration_transformation("border-left-width: 1px;",   "border-right-width: 1px;")
 
-    assert_declaration_transformation("border-color: #000 #111 #222 #333;",        "border-color: #000 #333 #222 #111;")
+    assert_declaration_transformation("border-color: #111 #222 #333 #444;",        "border-color: #111 #444 #333 #222;")
     assert_declaration_transformation("border-style: dotted solid double dashed;", "border-style: dotted dashed double solid;")
-    assert_declaration_transformation("border-width: 0px 1px 2px 3px;",            "border-width: 0px 3px 2px 1px;")
+    assert_declaration_transformation("border-width: 1px 2px 3px 4px;",            "border-width: 1px 4px 3px 2px;")
   end
 
   test "Should transform the clear/float properties" do
