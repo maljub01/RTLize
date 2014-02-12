a69b24f5260d5afb1799a071aada9a8b21ffd06d
Marwan Al Jubeh <marwan.aljubeh@gmail.com>
2014-02-13 01:35:55 +0200

    Removed dependency on Tilt

--- lib/rtlize/rtl_processor.rb
+++ lib/rtlize/rtl_processor.rb
@@ -1,14 +1,15 @@
-require 'tilt'
 require 'rtlize/rtlizer'
 
 module Rtlize
-  class RtlProcessor < Tilt::Template
-    self.default_mime_type = 'text/css'
+  class RtlProcessor
+    attr_reader :data
 
-    def prepare; end
+    def initialize(file, &block)
+      @data = block.call
+    end
 
-    def evaluate(scope, locals, &block)
-      if scope.pathname.basename.to_s.match(/\.rtl\.css/i)
+    def render(context, locals, &block)
+      if context.pathname.basename.to_s.match(/\.rtl\.css/i)
         Rtlize::RTLizer.transform(data)
       else
         data
